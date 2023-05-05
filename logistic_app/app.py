00205ec689306ae03d15be894221f78a8fa5a09b
igor-sb <84479009+igor-sb@users.noreply.github.com>
2023-05-04 20:46:24 -0700

    fix app

--- logistic_app/app.py
+++ logistic_app/app.py
@@ -7,26 +7,22 @@ from plotnine import (
 	geom_linerange,
 	labels,
 	scale_y_continuous,
+	theme_classic,
 )
 import numpy as np
 import pandas as pd
 from scipy.stats import uniform, bernoulli
 
 def logistic(x, x0, k):
-	return 1 / (1 + np.exp(-k*(x - x0)))
+	# return 1 / (1 + np.exp(-k*(x - x0)))
+	return 1 / (1 + np.exp(-(k*x - x0)))
 
-def log_loss(p_x, y):
-	return -y * np.log(p_x) - (1 - y) * np.log(1 - p_x)
-
-def create_test_data(n0s, n1s, x0, k):
+def create_test_data(n, x0, k):
 	df = pd.DataFrame({
-		'x': np.concatenate((
-			uniform.rvs(loc=0, scale=3, size=n0s),
-			uniform.rvs(loc=2, scale=3, size=n1s),
-		))
+		'x': uniform.rvs(loc=0, scale=5, size=n)
 	})
-	df['px_model'] = logistic(df['x'], x0, k)
-	df['y'] = bernoulli.rvs(df['px_model'])
+	df['probability_of_y_one'] = logistic(df['x'], x0, k)
+	df['y'] = bernoulli.rvs(df['probability_of_y_one'])
 	return df
 
 def calculate_px(x0, k):
@@ -42,7 +38,12 @@ def plot_data_and_px(df, px_df):
 		ggplot(df, aes('x', 'y'))
 		+ geom_line(data=px_df)
 		+ geom_point(size=4, shape='o', alpha=0.7, mapping=aes(fill='factor(y)'))
+		+ labels.ggtitle("logit(p(x)) = -k*(x - x0)")
+		+ theme_classic()
 	)
+
+def log_loss(p_x, y):
+	return -y * np.log(p_x) - (1 - y) * np.log(1 - p_x)
 
 def plot_logistic_fit(df, x0, k):
 	x = np.linspace(0, 5, 100)
@@ -67,18 +68,37 @@ def plot_logistic_fit(df, x0, k):
 		+ geom_point(size=4, shape='o', alpha=0.7)
 		+ scale_y_continuous(name='log loss')
 		+ labels.ggtitle("Total log-loss: " + str(np.sum(df['log_loss'])))
+		+ theme_classic()
 		# + ylim(0, 5)
 	)
 
 app_ui = ui.page_fluid(
-	ui.h2("Logistic regression fits"),
-	ui.input_numeric("x0", "x offset", value=2.5),
-	ui.input_numeric("k", "steepness", value=3),
-	ui.output_plot("logistic_plot"),
-	ui.h2("Model fit"),
-	ui.input_numeric("fit_x0", "x offset", value=2.5),
-	ui.input_numeric("fit_k", "steepness", value=3),
-	ui.output_plot("logistic_fits")
+	ui.row(
+		ui.column(
+			5,
+			ui.h2("Logistic regression fits"),
+			ui.row(
+				ui.input_numeric("seed", "Random seed", value=1, width="25%"),
+				ui.input_numeric("n", "# points", value=40, width="25%"),
+				ui.input_numeric("x0", "x offset (x0)", value=2.5, width="25%"),
+				ui.input_numeric("k", "steepness (k)", value=3, width="25%"),	
+			),
+			ui.output_plot("logistic_plot"),
+		),
+		ui.column(
+			5,
+			ui.h2("Model fit"),
+			ui.row(
+				ui.input_slider(
+					"fit_x0", "x offset (x0)", min=0, max=5, value=2.5, step=0.25, width="50%"
+				),
+				ui.input_slider(
+					"fit_k", "steepness (k)", min=0, max=10, value=3, step=0.25, width="50%"
+				),
+			),
+			ui.output_plot("logistic_fits"),
+		),	
+	)
 )
 
 
@@ -86,8 +106,8 @@ def server(input, output, session):
 
 	@reactive.Calc
 	def df():
-		np.random.seed(1)
-		return create_test_data(20, 20, input.x0(), input.k())
+		np.random.seed(input.seed())
+		return create_test_data(input.n(), input.x0(), input.k())
 
 	@output
 	@render.plot
