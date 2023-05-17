670d56ed946d64307842335aff70bfb3a81d336c
igor-sb <>
2023-05-16 20:51:42 -0700

    update plots

--- posts/logistic/plots.py
+++ posts/logistic/plots.py
@@ -15,10 +15,6 @@ def plot_naive_logistic_fit(sample_df, curve_df):
 			data=curve_df,
 			mapping=p9.aes(y='p_x')
 		)
-		# + p9.geom_linerange(
-		# 	mapping=p9.aes(ymin='p_x', ymax='y', color='factor(y)'),
-		# 	linetype='dashed'
-		# )
 		+ p9.geom_point(
 			mapping=p9.aes(y='y', fill='factor(y)'),
 			size=4,
